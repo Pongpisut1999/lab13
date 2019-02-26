77605623c0f34120eed59f5823c44cca33a40407
lab314-38@labeng.local <lab314-38@labeng.local>
2019-02-26 16:29:56 +0700

    gg

---
+++ lab13_1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+void swap(T d[],int x,int y){
+    T temp = d[x];
+    d[x] = d[y];
+    d[y] = temp;
+}
+
+template <typename T>
+void insertionSort(T d[],int N){
+	
+//Write your code here.
+   int x,y;
+   for(int i=0;i<N;i++){
+   	  if(i>=1){
+   	    x=d[i];
+   	    y=i;
+		 for(int l=0;l<N;l++){
+		 	if(l==i)
+			 cout <<"["<< d[l]<<"]" << " ";
+			else 
+			 cout << d[l] << " "; 
+		 }
+	  int j=i;
+	  while(j>0){
+   	  if(d[j]>d[j-1]){
+   	  	swap(d[j],d[j-1]);
+   	  	y--;
+	     }
+		j--;
+	   }
+	  if(i>=1){
+	  cout<<"=>";
+	  for(int k = 0; k < N; k++) {
+				if(k==y)
+				  cout<<'['<<d[k]<<']'<<" ";
+			    else
+				  cout << d[k] << " ";
+			}
+		}
+			cout<<endl;
+   }
+}
+}
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
+
